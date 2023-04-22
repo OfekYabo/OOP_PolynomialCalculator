d0bb00136eb3619b1e4ec4340a11fc16ee247c8b
OfekYabo <yaboofe@post.bgu.ac.il>
2023-04-22 21:38:01 +0300

    Monomial Class + Fixed Tests

---
+++ src/TMonomial.java
@@ -0,0 +1,41 @@
+public class TMonomial {
+
+
+    ///-------Monomial Function Tests--------
+
+    public static boolean add(String monomial1, String monomial2, String answer)
+    {
+        IMonomial n1 = Program.ConvertToMonomial(monomial1);
+        IMonomial n2 = Program.ConvertToMonomial(monomial2);
+        return answer.equals((n1.add(n2)).toString());
+    }
+    public static boolean mul(String monomial1, String monomial2, String answer)
+    {
+        IMonomial n1 = Program.ConvertToMonomial(monomial1);
+        IMonomial n2 = Program.ConvertToMonomial(monomial2);
+        return answer.equals((n1.mul(n2)).toString());
+    }
+    public static boolean evaluate(String monomial1, String scalar1, String answer)
+    {
+        IMonomial n1 = Program.ConvertToMonomial(monomial1);
+        Scalar n2 = Program.ConvertToScalar(scalar1);
+        return answer.equals(n2.power(n1.Exponent).mul(n1.Coefficient).toString());
+    }
+    public static boolean derivative(String monomial1, String answer)
+    {
+        IMonomial n1 = Program.ConvertToMonomial(monomial1);
+        return answer.equals(n1.derivative().toString());
+    }
+    public static boolean sign(String monomial1, String answer)
+    {
+        IMonomial n1 = Program.ConvertToMonomial(monomial1);
+        return answer.equals(String.valueOf(n1.sign()));
+    }
+    public boolean equals(String monomial1, String monomial2, boolean answer)
+    {
+        IMonomial n1 = Program.ConvertToMonomial(monomial1);
+        IMonomial n2 = Program.ConvertToMonomial(monomial2);
+        if (answer) return n1.equals(n2);
+        else return !n1.equals(n2);
+    }
+}
