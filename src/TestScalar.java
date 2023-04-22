6edda14a9dafa0b429906fa0cb07b6605d170ea1
OfekYabo <yaboofe@post.bgu.ac.il>
2023-04-22 19:36:08 +0300

    Monomial Class + Fixed Tests

--- src/TestScalar.java
+++ src/TestScalar.java
@@ -6,17 +6,46 @@ public class TestScalar {
         if (number.contains("/"))
         {
             String[] parts = number.split("/");
-            Rational n1 = new Rational(parseInt(parts[0], sign), parseInt(parts[1], sign));
+            Scalar n1 = new Rational(parseInt(parts[0], sign), parseInt(parts[1], sign));
             return n1;
         }
         else
         {
             int num1 = parseInt(number, sign);
-            Integer n1 = new Integer(num1);
+            Scalar n1 = new Integer(num1);
             return n1;
         }
     }
 
+    public static Monomial ConvertToMonomial(String number)
+    {
+        Monomial ans;
+        String[] arr = number.split("x");
+        if( arr.length == 1)
+        {
+            if (arr[0].contains("^"))
+            {
+                String exponent = arr[0].substring(1);
+                int sign = 1;
+                if (exponent.contains("-")) { sign = -1; }
+                return ans = new Monomial(parseInt(exponent, sign), new Integer(1));
+            }
+            else
+            {
+                return ans = new Monomial(1, ConvertToScalar(arr[0]));
+            }
+        }
+        else
+        {
+            String exponent = arr[1].substring(1);
+            int sign = 1;
+            if (exponent.contains("-")) { sign = -1; }
+            return ans = new Monomial(parseInt(exponent, sign), ConvertToScalar(arr[0]));
+        }
+    }
+
+
+///sign must be 1/-1
     public static int parseInt(String number, int sign) {
         int result = 0;
         for (int i = 0; i < number.length(); i++) {
