d3a2a8ad2df5beccf6ce69d6ce13ac44df17ad08
OfekYabo <yaboofe@post.bgu.ac.il>
2023-04-23 03:37:15 +0300

    Monomial Class + Fixed Tests

---
+++ Tests/Calculator/IntegerTest.java
@@ -0,0 +1,38 @@
+package Calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntegerTest {
+
+    @BeforeEach
+    void setUp() {
+        Integer i = new Integer(0);
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void mul() {
+    }
+
+    @Test
+    void neg() {
+    }
+
+    @Test
+    void power() {
+    }
+
+    @Test
+    void sign() {
+    }
+}
